3b8108a363f8081505c2bc4a0f60d32f2aeb5405
AmarnathBalaji <amarmarineblue@gmail.com>
2016-07-23 02:19:31 +0530

    initial activities - without functionalities

--- app/src/main/java/athreya/amrnath/placesmarkdown/MapsActivity.java
+++ app/src/main/java/athreya/amrnath/placesmarkdown/MapsActivity.java
@@ -1,10 +1,12 @@
 package athreya.amrnath.placesmarkdown;
 
+import android.app.Activity;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
